ffabdc679fc2eb5d9be58320c88152411e07f51a
Enzo Camilo Lado <enzo.camilo@laude.tech>
2024-12-04 13:08:10 +0100

    Add cards data and function to render cards

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+const cardsData = [
+    {
+        title:"Estrenos en Exclusiva",
+        description:"TIDAL ha estrenado en exclusiva los mejores álbumes de los dos últimos años y siempre con la mejor calidad de sonido.",
+        imageUrl:"./assets/images/cards/card-1.png"
+    },
+    {
+        title:"El mejor contenido audiovisual",
+        description:"Más de 200 mil videos musicales en alta definición, streaming de conciertos  en directo y series originales.",
+        imageUrl:"./assets/images/cards/card-2.png"
+    },
+    {
+        title:"Mucho más que música",
+        description:"Recomendaciones de conciertos según tus gustos y localización geográfica. Gana entradas para tus conciertos preferidos.",
+        imageUrl:"./assets/images/cards/card-3.png"
+    },
+]
+
+function renderCards() {
+    const container = document.querySelector('.cards-area');
+    console.log(container);
+    container.innerHTML = '';
+    cardsData.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.innerHTML = `
+            <img src="${card.imageUrl}" alt="${card.title}" class="card-image">
+            <div class="card-info">
+                <span class="card-title">${card.title}</span>
+                <span class="card-description">${card.description}</span>
+            </div>
+        `;
+        container.appendChild(cardElement);
+    });
+}
+document.addEventListener('DOMContentLoaded', renderCards);
